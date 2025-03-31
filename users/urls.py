630ee09f23f101aeb3513b3602e7f35b3d97e5e0
admin <shubhammunjal142@gmail.com>
2025-03-31 15:59:25 +1300

    [feature] login

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
+
 from django.urls import path
 from django.urls import path, include
 from mybackend.urls import urlpatterns
-from .views import RegisterView
+from .views import RegisterView, LoginView
 urlpatterns=[
     path('register/', RegisterView.as_view(), name='register'),
-   # path('login/', loginview.as_view()),
+   path('login/', LoginView.as_view() , name='login'),
     #path('logout/', logoutview.as_view()),
     #path('user/', UserView.as_view()),
 
