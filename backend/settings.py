178e728891eef17f48c5d836053b42cacd6531c5
admin <shubhammunjal142@gmail.com>
2025-04-03 00:32:39 +1300

    [update vercel

--- backend/settings.py
+++ backend/settings.py
@@ -162,3 +162,25 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',  # To ensure the user needs to be authenticated
     ]
 }
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React (local)
+    "https://ci-cd-assignment-frontend.vercel.app",  # Production frontend
+]
+# Allow credentials (important if using authentication)
+CORS_ALLOW_CREDENTIALS = True
+
+# Allowed HTTP methods (optional, but recommended)
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE",
+    "OPTIONS",
+]
+
+# Allowed headers (optional, but useful for Authorization headers)
+CORS_ALLOW_HEADERS = [
+    "authorization",
+    "content-type",
+]
+CORS_ALLOW_ALL_ORIGINS = True
